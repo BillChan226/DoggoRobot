0bfb06f04aa2760bfd6011a2c8b1bdf6c5ad1197
mason <zhao986@purdue.edu>
2023-03-21 07:44:50 +0800

    Ignore the test Add pid and change name to forward in pid file

--- doggo_PID.py
+++ doggo_PID.py
@@ -1,5 +1,6 @@
-#from safety_gym import safety_gym
+# from safety_gym import safety_gym
 import sys
+
 sys.path.append("/home/gong112/service_backup/work/zhaorun/doggo/safety-gym/")
 import safety_gym
 import gym
@@ -7,11 +8,11 @@ import gym.envs.registration as r
 import os
 import math
 import time
-#print(r)
+# print(r)
 
 import numpy as np
 
-    
+
 # class DoggoController:
 #     def __init__(self):
 #         self.phase = 0.0
@@ -88,7 +89,7 @@ import numpy as np
 #             #print("sss", np.norm(hip_y-ankle))
 
 #             #Calculate leg length and projection onto the x-y plane
-            
+
 #             # if hip_y[0] == hip_z[1]:
 #             #     print("hhhhhh")
 
@@ -114,7 +115,7 @@ import numpy as np
 #             action[leg_idx * 3 + 1] = np.clip(self.elevation_kp * elevation_error, -1, 1)
 #             action[leg_idx * 3 + 2] = np.clip(self.knee_kp * knee_error, -1, 1)
 
-            
+
 #             #print("error", )
 #         print("action", action)
 #         #action = np.random.rand(12)
@@ -124,25 +125,26 @@ import numpy as np
 class DoggoController:
     def __init__(self):
         self.phase_offsets = [0, math.pi / 2, math.pi, 3 * math.pi / 2]
-        self.amplitude = 0.2 # adjust this to control speed
-        self.frequency = 2 # adjust this to control stride frequency
-        #self.hip_z_phase = [0.5, -0.5, 0.5, -0.5]
-
+        self.amplitude = 0.2  # adjust this to control speed
+        self.frequency = 2  # adjust this to control stride frequency
+        # self.hip_z_phase = [0.5, -0.5, 0.5, -0.5]
 
         self.left = [0.1, 0, 0.1, 0]
         self.right = [0, 0.1, 0, 0.1]
-        #self.hip_z_phase = [0.1, 0.1, 0.1, 0.1]
+        # self.hip_z_phase = [0.1, 0.1, 0.1, 0.1]
         self.hip_y_phase = [0.1, 0.1, 0.1, 0.1]
         self.vel_pre = np.zeros(3)
         self.vel_start = 0
         self.flag = False
 
         self.kp = 1.5
-        self.ki = 0
-        self.kd = 0
+        self.ki = 1
+        self.kd = 0.5
+        self.dirzOld = np.zeros(4)
+        self.dirzSum = np.zeros(4)
 
-    def get_actions(self, observation):
-        
+    def get_actions_Forward(self, observation):
+
         action = np.zeros(12)
         vel_new = obs[101:104]
 
@@ -156,18 +158,18 @@ class DoggoController:
             hip_z_phase = self.right
 
         self.vel_pre = vel_new
-
+        dir_z = np.zeros(4)
         for i in range(4):
-            dir_z = observation[48+4*i] - hip_z_phase[i]
-            
-            action[i] = -np.clip(dir_z*self.kp, -1, 1)
+            dir_z[i] = observation[48 + 4 * i] - hip_z_phase[i]
+        self.dirzSum = np.add(dir_z, self.dirzSum)
+        for i in range(4):
+            action[i] = -np.clip(dir_z[i] * self.kp + self.dirzSum[i] *self.ki + (self.dirzOld[i]-dir_z[i])*self.kd, -1, 1)
 
-            dir_y = observation[46+4*i]# + self.hip_y_phase[i]
+            dir_y = observation[46 + 4 * i]  # + self.hip_y_phase[i]
 
-            action[i+4] = -np.clip(dir_y, -1, 1)
-            
-            action[i+8] = 0
+            action[i + 4] = -np.clip(dir_y, -1, 1)
 
+            action[i + 8] = 0
 
             # error1 = observation[48+4*i]-0.1
             # error2 = observation[50+4*i]-0
@@ -179,19 +181,18 @@ class DoggoController:
             # else:
             #     action[i] = 0
 
-    
             # if abs(error2) > 0.1:
             #     action[i+4] = -np.clip(error2, -1, 1)
             # else: action[i+4] = 0
 
-        #action[i+8] = 0
-            #print("hip_y", observation[46+4*i])
-            #print("hip_z", observation[48+4*i])
-            # print("ankle1", observation[38+4*i])
-            # print("ankle2", observation[39+4*i])
-            
-            #action[i+8] = 0 # match the hip motion for now
-            #print("error1", error1)
+        # action[i+8] = 0
+        # print("hip_y", observation[46+4*i])
+        # print("hip_z", observation[48+4*i])
+        # print("ankle1", observation[38+4*i])
+        # print("ankle2", observation[39+4*i])
+
+        # action[i+8] = 0 # match the hip motion for now
+        # print("error1", error1)
 
         return action
 
@@ -200,25 +201,23 @@ if __name__ == "__main__":
 
     env = gym.make('Safexp-DoggoGoal2-v0')
     obs = env.reset()
-    #model = PIDController()
+    # model = PIDController()
     model = DoggoController()
 
     start_time = time.time()
-    action = model.get_actions(obs)
-    
+    action = model.get_actions_Forward(obs)
+
     for i in range(1000):
-        
-        #print("obs", obs)
+        # print("obs", obs)
         # for sensor in env.sensors_obs:  # Explicitly listed sensors
         #         dim = env.robot.sensor_dim[sensor]
         #         print("obs-dim",sensor, dim)
-        #action = np.ones(12)
-        #print("action", action)
-        action = model.get_actions(obs)
-        
+        # action = np.ones(12)
+        # print("action", action)
+        action = model.get_actions_Forward(obs)
+
         obs, reward, done, info = env.step(action)
 
         env.render()
 
         print("velocimeter", obs[101:104])
-
