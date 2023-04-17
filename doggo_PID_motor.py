44c4d7283ab5f669c39ef6f12831da46f7cbc45e
mason <zhao986@purdue.edu>
2023-04-18 05:16:24 +0800

    motor pid working but overall notworking

--- doggo_PID_motor.py
+++ doggo_PID_motor.py
@@ -8,6 +8,7 @@ import gym.envs.registration as r
 import os
 import math
 import time
+import matplotlib.pyplot as plt
 # print(r)
 
 import numpy as np
@@ -26,68 +27,46 @@ class DoggoController:
         self.old_error = np.zeros(12)
 
         self.cumulative_error = np.zeros(12)
-        self.kp = np.array([1.5, 1.5, 1.5, 1.5, 3, 3, 3, 3, 3, 3, 3, 3])
-        self.ki = np.array([0.1, 0.1, 0.1, 0.1, 0.3, 0.3, 0.3, 0.3, 0.2, 0.2, 0.2, 0.2])
-        self.kd = np.array([0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5])
+        self.kp = np.zeros(12)
+        self.ki = np.zeros(12)
+        self.kd = np.zeros(12)
+        self.kp = np.array([0.4, 0.4, 0.4, 0.4, 1.2, 1.2, 1.2, 1.2, 1.5, 1.5, 1.5, 1.5])
+        self.ki = np.array([0.0005, 0.0005, 0.0005, 0.0005, 0.015, 0.015, 0.015, 0.015, 0.008, 0.008, 0.008, 0.008])
+        self.kd = np.array([-0.06, -0.06, -0.06, -0.06, -0.15, -0.15, -0.15, -0.15, -0.3, -0.3, -0.3, -0.3])
+        # self.kp[4] = 1.2
+        # self.ki[4] = 0.015
+        # self.kd[4] = -0.15
+        # self.kp[0] = 0.4
+        # self.ki[0] = 0.0005
+        # self.kd[0] = -0.06
+        # self.kp[8] = 1.5
+        # self.ki[8] = 0.008
+        # self.kd[8] = -0.3
+        # self.ki = np.array([0.1, 0.1, 0.1, 0.1, 0.3, 0.3, 0.3, 0.3, 0.2, 0.2, 0.2, 0.2])
+        # self.kd = np.array([0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5])
         self.base = np.array([10, 10, 10, 10, -30, 67.5, 67.5, -30, -37.5, -37.5, -37.5, -37.5])
-        self.halfrange = np.array([10, 10, 10, 10, 45, 67.5, 67.5, 45, 37.5, 37.5, 37.5, 37.5])
+        self.halfrange = np.array([20, 20, 20, 20, 45, 67.5, 67.5, 45, 37.5, 37.5, 37.5, 37.5])
+        self.errorList = []
+        self.actionList=[]
+        self.expectList = []
         # self.motorDir = {"hip_z":0,"hip_y":1,"ankle":2}
         # self.legDir = {"fl":0,"fr":9, "bl":3,"br":6}
 
         
 
-    # def get_actions_Motor(self, value,expect, motor):
-    #     value = np.degrees(value)
-    #     if motor % 3 == 0:
-    #         #hip_z
-    #         value = (value - 10) / 20
-    #         value = np.clip(value, -1, 1)
-    #         error = expect - value
-    #         self.cumulative_error[motor] += error
-    #         errorchange = self.old_error[motor] - error
-    #         self.old_error[motor] = error
-    #         action = error * self.hip_z_p
-    #         action += self.cumulative_error[motor] * self.hip_z_i
-    #         action += errorchange * self.hip_Z_d
-    #         action = np.clip(action, -1, 1)
-    #     if motor % 3 == 1:
-    #         # hip_y
-    #         if motor == 1 or motor == 10:
-    #             value = (value + 30) / 45
-    #         else:
-    #             value = (value - 67.5) / 67.5
-    #         value = np.clip(value, -1, 1)
-    #         error = expect - value
-    #         self.cumulative_error[motor] += error
-    #         errorchange = self.old_error[motor] - error
-    #         self.old_error[motor] = error
-    #         action = error * self.hip_y_p
-    #         action += self.cumulative_error[motor] * self.hip_y_i
-    #         action += errorchange * self.hip_y_d
-    #         action = np.clip(action, -1, 1)
-    #     if motor % 3 == 2:
-    #         # ankle
-    #         value = (value + 37.5) / 37.5
-    #         value = np.clip(value, -1, 1)
-    #         error = expect - value
-    #         self.cumulative_error[motor] += error
-    #         errorchange = self.old_error[motor] - error
-    #         self.old_error[motor] = error
-    #         action = error * self.ankle_p
-    #         action += self.cumulative_error[motor] * self.ankle_i
-    #         action += errorchange * self.ankle_d
-    #         action = np.clip(action, -1, 1)
-    #
-    #
-    #
-    #     return action
     def get_action(self,expect, actual):
+        actual = (actual - self.base) / self.halfrange
+        # expect = -0.5
         error = expect-actual
-        error = (error - self.base)/self.halfrange
+        self.expectList.append(actual[4])
+
+        # print(error)
+        self.errorList.append(error[8])
         errorChange = self.old_error-error
         self.cumulative_error += error
         self.old_error = error
         action = self.kp*error + self.ki* self.cumulative_error + self.kd*errorChange
+        self.actionList.append(action[0])
         return np.clip(action, -1, 1)
 
 
@@ -100,7 +79,7 @@ if __name__ == "__main__":
     model = DoggoController()
     # motorIndex = [48,52,56,60,46,50,54,58,38,40,42,44]
     motorIndex = [10, 14, 18, 22, 8, 12, 16, 20, 0, 2, 4, 6]
-    motorTrans = np.zeros(24,24)
+    motorTrans = np.zeros((12,24))
     for i in range(12):
         motorTrans[i][motorIndex[i]]=1
 
@@ -111,22 +90,29 @@ if __name__ == "__main__":
     test = "Left"
     # test = "Right"
 
-    for i in range(1000):
+    for i in range(200):
 
         # TODO: ADD the model to train return as targetArray
         targetArray = np.zeros(12)
-        state = np.arcsin(motorTrans.dot(obs[38:62]))
-
-        action = model.get_action(state)
+        state = np.degrees(np.arcsin(motorTrans.dot(obs[38:62])))
+        # state = motorTrans.dot(state)
+        action = model.get_action(targetArray,state)
         # for i in range(12):
         #     print(obs[motorIndex[i]])
         #     if obs[motorIndex[i]+1] ==0:
         #         action.append(model.get_actions_Motor(np.pi/2, targetArray[i], i))
         #     else:
         #         action.append(model.get_actions_Motor(np.arctan(obs[motorIndex[i]]/obs[motorIndex[i+1]]),targetArray[i],i))
-        print(action)
+        # print(action)
         obs, reward, done, info = env.step(action)
 
         env.render()
 
-        print("velocimeter", obs[101:104])
+        # print("velocimeter", obs[101:104])
+    plt.figure(0)
+    plt.plot(model.errorList)
+    # plt.figure(1)
+    # plt.plot(model.actionList)
+    # plt.figure(2)
+    # plt.plot(model.expectList)
+    plt.show()
