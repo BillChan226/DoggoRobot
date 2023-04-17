8ebd65b7e4d31b1824bbad6b0a9a73ea38753589
mason <zhao986@purdue.edu>
2023-04-17 12:18:01 +0800

    PID structure is build, haven't find way to properly tune the constant available for future training.

--- doggo_PID_motor.py
+++ doggo_PID_motor.py
@@ -14,67 +14,82 @@ import numpy as np
 
 class DoggoController:
     def __init__(self):
-        self.ankle_p = 3
-        self.ankle_i = 0.2
-        self.ankle_d = 0.5
-        self.hip_y_p = 3
-        self.hip_y_i = 0.3
-        self.hip_y_d = 0.5
-        self.hip_z_p = 1.5
-        self.hip_z_i = 0.1
-        self.hip_Z_d = 0.5
+        # self.ankle_p = 3
+        # self.ankle_i = 0.2
+        # self.ankle_d = 0.5
+        # self.hip_y_p = 3
+        # self.hip_y_i = 0.3
+        # self.hip_y_d = 0.5
+        # self.hip_z_p = 1.5
+        # self.hip_z_i = 0.1
+        # self.hip_Z_d = 0.5
         self.old_error = np.zeros(12)
+
         self.cumulative_error = np.zeros(12)
-        self.motorDir = {"hip_z":0,"hip_y":1,"ankle":2}
-        self.legDir = {"fl":0,"fr":9, "bl":3,"br":6}
+        self.kp = np.array([1.5, 1.5, 1.5, 1.5, 3, 3, 3, 3, 3, 3, 3, 3])
+        self.ki = np.array([0.1, 0.1, 0.1, 0.1, 0.3, 0.3, 0.3, 0.3, 0.2, 0.2, 0.2, 0.2])
+        self.kd = np.array([0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5])
+        self.base = np.array([10, 10, 10, 10, -30, 67.5, 67.5, -30, -37.5, -37.5, -37.5, -37.5])
+        self.halfrange = np.array([10, 10, 10, 10, 45, 67.5, 67.5, 45, 37.5, 37.5, 37.5, 37.5])
+        # self.motorDir = {"hip_z":0,"hip_y":1,"ankle":2}
+        # self.legDir = {"fl":0,"fr":9, "bl":3,"br":6}
 
         
 
-    def get_actions_Motor(self, value,expect, motor):
-        value = np.degrees(value)
-        if motor % 3 == 0:
-            #hip_z
-            value = (value - 10) / 20
-            value = np.clip(value, -1, 1)
-            error = expect - value
-            self.cumulative_error[motor] += error
-            errorchange = self.old_error[motor] - error
-            self.old_error[motor] = error
-            action = error * self.hip_z_p
-            action += self.cumulative_error[motor] * self.hip_z_i
-            action += errorchange * self.hip_Z_d
-            action = np.clip(action, -1, 1)
-        if motor % 3 == 1:
-            # hip_y
-            if motor == 1 or motor == 10:
-                value = (value + 30) / 45
-            else:
-                value = (value - 67.5) / 67.5
-            value = np.clip(value, -1, 1)
-            error = expect - value
-            self.cumulative_error[motor] += error
-            errorchange = self.old_error[motor] - error
-            self.old_error[motor] = error
-            action = error * self.hip_y_p
-            action += self.cumulative_error[motor] * self.hip_y_i
-            action += errorchange * self.hip_y_d
-            action = np.clip(action, -1, 1)
-        if motor % 3 == 2:
-            # ankle
-            value = (value + 37.5) / 37.5
-            value = np.clip(value, -1, 1)
-            error = expect - value
-            self.cumulative_error[motor] += error
-            errorchange = self.old_error[motor] - error
-            self.old_error[motor] = error
-            action = error * self.ankle_p
-            action += self.cumulative_error[motor] * self.ankle_i
-            action += errorchange * self.ankle_d
-            action = np.clip(action, -1, 1)
+    # def get_actions_Motor(self, value,expect, motor):
+    #     value = np.degrees(value)
+    #     if motor % 3 == 0:
+    #         #hip_z
+    #         value = (value - 10) / 20
+    #         value = np.clip(value, -1, 1)
+    #         error = expect - value
+    #         self.cumulative_error[motor] += error
+    #         errorchange = self.old_error[motor] - error
+    #         self.old_error[motor] = error
+    #         action = error * self.hip_z_p
+    #         action += self.cumulative_error[motor] * self.hip_z_i
+    #         action += errorchange * self.hip_Z_d
+    #         action = np.clip(action, -1, 1)
+    #     if motor % 3 == 1:
+    #         # hip_y
+    #         if motor == 1 or motor == 10:
+    #             value = (value + 30) / 45
+    #         else:
+    #             value = (value - 67.5) / 67.5
+    #         value = np.clip(value, -1, 1)
+    #         error = expect - value
+    #         self.cumulative_error[motor] += error
+    #         errorchange = self.old_error[motor] - error
+    #         self.old_error[motor] = error
+    #         action = error * self.hip_y_p
+    #         action += self.cumulative_error[motor] * self.hip_y_i
+    #         action += errorchange * self.hip_y_d
+    #         action = np.clip(action, -1, 1)
+    #     if motor % 3 == 2:
+    #         # ankle
+    #         value = (value + 37.5) / 37.5
+    #         value = np.clip(value, -1, 1)
+    #         error = expect - value
+    #         self.cumulative_error[motor] += error
+    #         errorchange = self.old_error[motor] - error
+    #         self.old_error[motor] = error
+    #         action = error * self.ankle_p
+    #         action += self.cumulative_error[motor] * self.ankle_i
+    #         action += errorchange * self.ankle_d
+    #         action = np.clip(action, -1, 1)
+    #
+    #
+    #
+    #     return action
+    def get_action(self,expect, actual):
+        error = expect-actual
+        error = (error - self.base)/self.halfrange
+        errorChange = self.old_error-error
+        self.cumulative_error += error
+        self.old_error = error
+        action = self.kp*error + self.ki* self.cumulative_error + self.kd*errorChange
+        return np.clip(action, -1, 1)
 
-
-
-        return action
 
 
 if __name__ == "__main__":
@@ -83,7 +98,11 @@ if __name__ == "__main__":
     obs = env.reset()
     # model = PIDController()
     model = DoggoController()
-    motorIndex = [48,52,56,60,46,50,54,58,38,40,42,44]
+    # motorIndex = [48,52,56,60,46,50,54,58,38,40,42,44]
+    motorIndex = [10, 14, 18, 22, 8, 12, 16, 20, 0, 2, 4, 6]
+    motorTrans = np.zeros(24,24)
+    for i in range(12):
+        motorTrans[i][motorIndex[i]]=1
 
     start_time = time.time()
     # action = model.get_actions_Forward(obs)
@@ -96,13 +115,15 @@ if __name__ == "__main__":
 
         # TODO: ADD the model to train return as targetArray
         targetArray = np.zeros(12)
-        action = []
-        for i in range(12):
-            print(obs[motorIndex[i]])
-            if obs[motorIndex[i+1]] ==0:
-                action.append(model.get_actions_Motor(np.pi/2, targetArray[i], i))
-            else:
-                action.append(model.get_actions_Motor(np.arctan(obs[motorIndex[i]]/obs[motorIndex[i+1]]),targetArray[i],i))
+        state = np.arcsin(motorTrans.dot(obs[38:62]))
+
+        action = model.get_action(state)
+        # for i in range(12):
+        #     print(obs[motorIndex[i]])
+        #     if obs[motorIndex[i]+1] ==0:
+        #         action.append(model.get_actions_Motor(np.pi/2, targetArray[i], i))
+        #     else:
+        #         action.append(model.get_actions_Motor(np.arctan(obs[motorIndex[i]]/obs[motorIndex[i+1]]),targetArray[i],i))
         print(action)
         obs, reward, done, info = env.step(action)
 
